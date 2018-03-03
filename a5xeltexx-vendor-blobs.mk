b54d53f0c2df883e9730f0f051e4ed6b13c15fe6
Victor Lourme <l0w@protonmail.ch>
2018-03-03 12:10:18 +0100

    a5xeltexx: update fingerprint from A510FDODD3BQA4

--- a5xeltexx-vendor-blobs.mk
+++ a5xeltexx-vendor-blobs.mk
@@ -131,7 +131,7 @@ PRODUCT_COPY_FILES += \
 ##########################
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/proprietary/lib/hw/fingerprint.universal7580.so:system/lib/hw/fingerprint.universal7580.so \
+    $(LOCAL_PATH)/proprietary/lib/hw/fingerprint.default.so:system/lib/hw/fingerprint.default.so \
     $(LOCAL_PATH)/proprietary/lib/libbauthserver.so:system/lib/libbauthserver.so \
     $(LOCAL_PATH)/proprietary/lib/libbauthtzcommon.so:system/lib/libbauthtzcommon.so \
     $(LOCAL_PATH)/proprietary/lib/libegis_fp_normal_sensor_test.so:system/lib/libegis_fp_normal_sensor_test.so \
